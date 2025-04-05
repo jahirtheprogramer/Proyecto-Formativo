595700f303ad99b753dd80e630a21aedce0fc61c
jahir vera <kevinnveralopez@gmail.com>
2025-04-05 15:00:28 -0500

    Proyecto formativo
    Se encuentra el link del documento del proyecto formativo.

---
+++ Proyecto formativo.bat
@@ -0,0 +1,3 @@
+https://sena4-my.sharepoint.com/personal/lfossi_sena_edu_co/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Flfossi%5Fsena%5Fedu%5Fco%2FDocuments%2FFichas%20Instructor%2FProyectos%20de%20formaci%C3%B3n%20Etapa%20Lectiva%2FFicha%202849024%2FGrupo%205%20%2D%20Inventario%20Tiendas&ga=1
+
+link del proyecto formativo
